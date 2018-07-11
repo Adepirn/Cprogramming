7020a9b84d7ff241525507e405196c6c9e98b89d
student <student@localhost.localdomain>
2018-07-11 14:29:26 -0500

    stuff

---
+++ getCIO.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int input = 0;
+    printf("Enter a character: ");
+    input = getc(stdin);
+    printf("Your character was: ");
+    putc(input, stdout);
+    printf("\n");
+    
+
+    return 0;
+}
