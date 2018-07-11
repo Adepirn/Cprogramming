1a5e973336e94245d644fecb0a0a4e3f221ba6da
student <student@localhost.localdomain>
2018-07-11 08:33:34 -0500

    finished arrayLab3a.c

--- arrayLab3a.c
+++ arrayLab3a.c
@@ -1,37 +1,25 @@
+/*  Lab3a
+*	Mahaffey, Richard
+*	11 July 2018
+*/
+
+
 #include <stdio.h>
 
 int main()
 {
-	int studentAge[13] = {0};
+	int studentAge[13] = {27, 26, 34, 21, 37, 32, 29, 31, 34, 25, 28, 31, 30};
 	int i = 0;
-	char saying[] = {'G', 'o', '\n', 'A', 'w', 'a', 'y', '\0'};
+	char saying[] = {"life\nis\nstrange\n"};
 
-
-	studentAge [0] = 27;
-	studentAge [1] = 26;
-	studentAge [2] = 34;
-	studentAge [3] = 21;
-	studentAge [4] = 37;
-	studentAge [5] = 32;
-	studentAge [6] = 29;
-	studentAge [7] = 31;
-	studentAge [8] = 34;
-	studentAge [9] = 25;
-	studentAge [10] = 28;
-	studentAge [11] = 31;
-	studentAge [12] = 30;
 
 	for (i=0; i < 13; i++)
 	{
 		printf("Contents of student age are %d \n", studentAge[i]);
 	}
 
-	for (i=0; i < 8; i++)
-	{
-		printf("%c", saying[i]);
-	}
+	printf("%s /n", saying);
 
-	printf("\n");
 
 	return 0;
 
